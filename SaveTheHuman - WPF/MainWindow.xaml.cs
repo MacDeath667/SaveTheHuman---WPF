de4ea46cdd906d208dedb414270d8871b9385a60
MacDeath667 <rom12.93@bk.ru>
2018-05-20 22:03:08 +0300

    It Works! Need to clean code

--- SaveTheHuman - WPF/MainWindow.xaml.cs
+++ SaveTheHuman - WPF/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,6 +15,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace SaveTheHuman___WPF
 {
@@ -25,15 +25,66 @@ namespace SaveTheHuman___WPF
     public partial class MainWindow : Window
     {
         Random random = new Random();
-
+        DispatcherTimer enemyTimer = new DispatcherTimer();
+        DispatcherTimer targetTimer = new DispatcherTimer();
+        bool humanCaptured = false;
         public MainWindow()
         {
             InitializeComponent();
+            GameOverText.Visibility = Visibility.Hidden;
+            enemyTimer.Tick += EnemyTimer_Tick;
+            enemyTimer.Interval = TimeSpan.FromSeconds(2);
+            human.Visibility = Visibility.Visible;
+            targetTimer.Tick += TargetTimer_Tick;
+            targetTimer.Interval = TimeSpan.FromSeconds(1);
         }
 
-        private void startButton_Click(object sender, RoutedEventArgs e)
+        void TargetTimer_Tick(object sender, EventArgs e)
+        {
+            ProgressBar.Value += 1;
+            if (ProgressBar.Value >= ProgressBar.Maximum)
+                EndTheGame();
+        }
+
+        private void EndTheGame()
+        {
+            if (!playArea.Children.Contains(GameOverText))
+            {
+                enemyTimer.Stop();
+                targetTimer.Stop();
+                humanCaptured = false;
+                human.Visibility = Visibility.Hidden;
+                GameOverText.Visibility = Visibility.Visible;
+                StartButton.Visibility = Visibility.Visible;
+                playArea.Children.Add(GameOverText);
+
+
+            };
+        }
+
+        void EnemyTimer_Tick(object sender, EventArgs e)
         {
             AddEnemy();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartGame();
+            //AddEnemy();
+        }
+
+        private void StartGame()
+        {
+            human.IsHitTestVisible = true;
+            humanCaptured = false;
+            ProgressBar.Value = 0;
+            StartButton.Visibility = Visibility.Collapsed;
+            playArea.Children.Clear();
+            playArea.Children.Add(human);
+            playArea.Children.Add(target);
+            enemyTimer.Start();
+            targetTimer.Start();
+
         }
 
         private void AddEnemy()
@@ -42,9 +93,17 @@ namespace SaveTheHuman___WPF
             {
                 Template = Resources["EnemyTemplate"] as ControlTemplate
             };
-            AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "Canvas.Left");
-            AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 100), random.Next((int)playArea.ActualHeight - 100), "Canvas.Top");
+            AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
+            AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 100), random.Next((int)playArea.ActualHeight - 100), "(Canvas.Top)");
             playArea.Children.Add(enemy);
+
+            enemy.MouseEnter += Enemy_MouseEnter;
+        }
+
+        private void Enemy_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (humanCaptured)
+                EndTheGame();
         }
 
         private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
@@ -62,5 +121,65 @@ namespace SaveTheHuman___WPF
             storyboard.Begin();
 
         }
+
+        private void human_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (enemyTimer.IsEnabled)
+            {
+                humanCaptured = true;
+                human.IsHitTestVisible = false;
+            }
+        }
+
+        private void target_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (targetTimer.IsEnabled && humanCaptured)
+            {
+                ProgressBar.Value = 0;
+                Canvas.SetLeft(target, random.Next(100, (int)playArea.ActualWidth - 100));
+                Canvas.SetTop(target, random.Next(100, (int)playArea.ActualHeight - 100));
+                Canvas.SetLeft(human, random.Next(100, (int)playArea.ActualWidth - 100));
+                Canvas.SetTop(human, random.Next(100, (int)playArea.ActualHeight - 100));
+                humanCaptured = false;
+                human.IsHitTestVisible = true;
+            }
+        }
+
+        private void playArea_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (humanCaptured)
+            {
+                Point pointerPosition = e.GetPosition(null);
+                Point relativePosition = grid.TransformToVisual(playArea).Transform(pointerPosition);
+                if ((Math.Abs(relativePosition.X - Canvas.GetLeft(human)) > human.ActualWidth * 3) || (Math.Abs(relativePosition.Y - Canvas.GetTop(human)) > human.ActualHeight * 3))
+                {
+                    humanCaptured = false;
+                    human.IsHitTestVisible = true;
+                }
+                else
+                {
+                    Canvas.SetLeft(human, relativePosition.X - human.ActualWidth / 2);
+                    Canvas.SetTop(human, relativePosition.Y - human.ActualHeight / 2);
+                }
+
+            }
+        }
+        private void playArea_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (humanCaptured)
+                EndTheGame();
+        }
+
+       
+
+
+
+
+
+
+        //private void grid_MouseMove(object sender, MouseEventArgs e)
+        // {
+
+        //}
     }
 }
